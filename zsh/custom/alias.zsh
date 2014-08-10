f4d73e526e7e11aac3f0a0bbb07e77d2ae2f141b
Travers McInerney <travers@mcinerney.me>
2014-08-09 18:26:52 -0700

    Alias git to hub

--- zsh/custom/alias.zsh
+++ zsh/custom/alias.zsh
@@ -2,3 +2,6 @@ alias mysql="mysql -uroot"
 alias reload!="source ~/.zshrc"
 alias restart!="touch ./tmp/restart.txt"
 alias watch="tail -f log/development.log"
+
+# Alias git commands to hub
+eval "$(hub alias -s)"
