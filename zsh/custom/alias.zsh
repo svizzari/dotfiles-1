abe4319de05a5a6886103a3c0c3b220bd846069b
Travers McInerney <travers@mcinerney.me>
2014-10-08 14:43:48 +1100

    Less is more. Upload tmux configuration on SSH

--- zsh/custom/alias.zsh
+++ zsh/custom/alias.zsh
@@ -1,9 +1,11 @@
 # General
+alias less="more"
 alias reload="source ~/.zshrc"
 alias ssho="/usr/bin/ssh"
 
 ssh() {
-  /usr/bin/ssh $* -t 'tmux a || tmux || /usr/bin/zsh'
+  rsync -avz -e ssh --copy-links --progress ~/.tmux.conf $1:~/ > /dev/null 2>&1
+  /usr/bin/ssh -A $* -t 'tmux a || tmux || `which zsh`'
 }
 
 # Ruby
